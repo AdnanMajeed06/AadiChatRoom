dc3c1120a5e8c872006347b0c5aa181d9be06a0c
TimeLine <adnan.majeed@timeline.ae>
2019-09-12 11:42:13 +0400

    Add code

---
+++ AadiChatRoom/AadiChatRoom.swift
@@ -0,0 +1,23 @@
+//
+//  AadiChatRoom.swift
+//  AadiChatRoom
+//
+//  Created by Adnan Majeed on 9/12/19.
+//  Copyright © 2019 TimeLine. All rights reserved.
+//
+
+import Foundation
+
+public final class AadiChatRoom {
+    
+    let name = "AadiChatRoom"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
